299d1f42eaf3e5934f887f5d25fcf6d540574df6
otienojoseph <otienoj.cruz@gmail.com>
2024-11-15 19:12:39 +0300

    Add authentication class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+from typing import TypeVar, List
+from flask import request
+"""Authentication file"""
+
+
+class Auth:
+    """
+    Class to manage API Authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require_auth"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """authorization_header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current_user"""
+        return None
