e81c440381634312a7f299292c4e169d33bcfb7b
otienojoseph <otienoj.cruz@gmail.com>
2024-11-15 18:40:03 +0300

    Add forbidden route

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -32,3 +32,11 @@ def unauthorized() -> str:
     Return: unauthorized
     """
     return abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return: Forbiden when trying to access resource but authenticated
+    """
+    return abort(403)
