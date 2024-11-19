14a0a5f9c49eb594f5e3bc80f3d33ca7279f165c
otienojoseph <otienoj.cruz@gmail.com>
2024-11-19 16:16:06 +0300

    Add basic_auth class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,6 +4,7 @@ Route module for the API
 """
 from os import getenv
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -17,6 +18,8 @@ auth = None
 
 if os.environ.get('AUTH_TYPE') == 'auth':
     auth = Auth()
+elif os.environ.get('AUTH_TYPE') == 'basic_auth':
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
